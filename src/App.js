e038027546d3f5170b67a387a2e9d286968b3cde
Alferid69 <nsr-1452-14@amu.edu.et>
2024-07-10 01:03:07 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./bootstrap.min.css";
+import "./bootstrap.min.css.map";
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  function handleAddTodos(newToDo) {
+    setTodos((todos) => [...todos, newToDo]);
+    console.log(todos[0]);
+  }
+
+  function handleDelete(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  function handleReset() {
+    const comfirmDelete = window.confirm("Are you sure you want to clear the list?")
+    if(comfirmDelete) setTodos([]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <div className="col-12 bg-secondary p-3">
+        <Form onAddTodo={handleAddTodos} />
+      </div>
+      {todos.length < 1 ? null : (
+        <div className="bg-success mt-3">
+          <ToDoLists
+            todos={todos}
+            onDelete={handleDelete}
+            onReset={handleReset}
+          />
+        </div>
+      )}
     </div>
   );
 }
 
-export default App;
+function Form({ onAddTodo }) {
+  const [text, setText] = useState("");
+  const newToDo = {
+    id: crypto.randomUUID(),
+    text,
+  };
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onAddTodo(newToDo);
+    setText("");
+  }
+
+  return (
+    <form
+      className="form d-flex align-items-center flex-column"
+      onSubmit={handleSubmit}
+    >
+      <h1>Welcome to ToDo list app</h1>
+      <div className="col-7 m-4">
+        <input
+          type="text"
+          className="form-control"
+          value={text}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="col-4">
+        <button className="form-control bg-primary text-light">Add</button>
+      </div>
+    </form>
+  );
+}
+
+function ToDoLists({ todos, onDelete, onReset }) {
+  return (
+    <div className="col p-3">
+      <ul className="list-group">
+        {todos.map((todo) => (
+          <ToDo
+            text={todo.text}
+            onDelete={onDelete}
+            
+            id={todo.id}
+            key={todo.id}
+          />
+        ))}
+        <ToDo />
+      </ul>
+      <div className="row d-flex align-items-center justify-content-center mt-3">
+        <button className="bg-danger col-4 button" onClick={onReset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+function ToDo({ text, id, onDelete }) {
+  function handleDelete() {
+    const comfirmDelete = window.confirm(
+      "Are you sure you want to remove the item?"
+    );
+
+    if (comfirmDelete) onDelete(id);
+  }
+  return (
+    text && (
+      <li
+        className="list-group-item d-flex"
+        style={{ backgroundColor: "#17a2b8" }}
+      >
+        <h4>{text}</h4>
+        <button className="bg-danger" onClick={handleDelete}>
+          ✖️
+        </button>
+      </li>
+    )
+  );
+}
